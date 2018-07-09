c15749c5998eb0c409a6edc4fe91e5d47b641b44
flavianery <flavia_pineiro@hotmail.com>
2018-07-09 03:42:07 -0300

    Update tec_assistiva_chapeu.ino

--- tec_assistiva_chapeu.ino
+++ tec_assistiva_chapeu.ino
@@ -13,22 +13,23 @@ Ultrasonic ultrassom1(trig1,echo1); // define o nome do sensor(ultrassom)
 // Esta função "setup" roda uma vez quando a placa e ligada ou resetada
 void setup() {
    pinMode(motor1,OUTPUT);
+   Serial.begin(9600);
  }
 
 // Função que se repete infinitamente quando a placa é ligada
 void loop() {
-   dist1 = ultrassom1.distanceRead();// ultrassom.distanceRead() retorna a             // distancia em centímetros 
+   dist1 = ultrassom1.distanceRead();// ultrassom.distanceRead() retorna a distancia em centímetros 
+   Serial.println(dist1);
 
-  //Restringe a distância ao intervalo determinado
-   if(dist1 < mindist){dist1 = mindist;}
-   if(dist1 > maxdist){dist1 = maxdist;}  
+  //Restringe a distância ao intervalo determinado
+   if(dist1 < mindist && dist1 > 3){dist1 = mindist;}
+   if(dist1 > maxdist){dist1 = maxdist;}
 
   //Define intensidade de vibração dos motores de acordo com a distância
-   if(dist1 <= trigdist){ 
-      v1 = map(dist1, mindist, trigdist, 255, 0);
-      analogWrite(motor1,v1);
-   }
-
-   delay(50);
+    if(dist1 <= trigdist){ 
+      v1 = map(dist1, mindist, trigdist, 255, 0);
+      analogWrite(motor1,v1);
+  }
+  delay(50);
 } 
 
